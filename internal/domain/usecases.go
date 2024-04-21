10bddd1be97f3c1e19c1ef5636933edb4489f684
Boobeh Pugsheen <bobby@192.168.1.125>
2024-04-21 21:19:46 +0100

    more stuff

---
+++ internal/domain/usecases.go
@@ -0,0 +1,31 @@
+package domain
+
+import "time"
+
+func SaveEnergyPrices(energyPrices []EnergyPrice, persist func(EnergyPrice) error) []error {
+	errs := []error{}
+	for _, energyPrice := range energyPrices {
+		err := persist(energyPrice)
+		errs = append(errs, err)
+	}
+	return errs
+}
+
+func EnergyPriceToEnergyUsage(energyPrice EnergyPrice) EnergyUsage {
+	panic("Not implemented yet")
+}
+
+type EnergyPriceRepository interface {
+	Save(EnergyPrice) error
+	Get(time.Time) (EnergyPrice, error)
+}
+
+type NotificationSender func(EnergyUsage) error
+
+func mapTo[T any, U any](slice []T, mappingFunc func(T) U) []U {
+	u := []U{}
+	for _, t := range slice {
+		u = append(u, mappingFunc(t))
+	}
+	return u
+}
